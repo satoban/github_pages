1c954ebe0e72e6a9d15a3ca4d904b5edaaf1c1ae
satohan\satomu <nekochain9901@gmail.com>
2022-11-07 14:55:04 +0900

    時間および分が１桁だった場合に０を付けて２桁表示にする

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+/*use strictに設定することでエラーを厳しくチェックする */
 'use strict';
-//年月日を取得する
+
+/*年月日を取得する*/
 const now = new Date();//Dateオブジェクトを現在日時で初期化
 const year = now.getFullYear();//年
-const month = now.getMonth();//月
-const date = now.getDate();//日付
-const hour = now.getHours();//時間
-const min = now.getMinutes();//分
-//テンプレート文字列で出力
-const output = `${year}/${month + 1}/${date} ${hour}:${min}`;
-document.getElementById('time').textContent = output;
+let month = now.getMonth();//月
+let date = now.getDate();//日付
+let hour = now.getHours();//時間
+let min = now.getMinutes();//分 
+
+/*1桁なら0を足して2桁表示にする*/
+//条件3項演算子 --> 条件式 ? Trueの処理 : Falseの処理
+month = (month < 10) ? "0" + month : month;
+date = (date < 10) ? "0" + date : date;
+hour = (hour < 10) ? "0" + hour : hour;
+min = (min < 10) ? "0" + min : min;
+
+/*テンプレート文字列で出力*/
+let output = `${year}/${month + 1}/${date} ${hour}:${min}`;
+document.getElementById('time').textContent = output;//id = "time" に一致する要素に変数outputを渡す
